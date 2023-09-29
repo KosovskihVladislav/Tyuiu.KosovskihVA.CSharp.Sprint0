f27148c5e2df765ee6084b6bf877ca57609c846f
Tyuiu.kosovskihVA@outlook.com <mr.kosovskih35@mail.ru>
2023-09-30 00:05:39 +0500

    Task5

---
+++ Tyuiu.KosovskihVA.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KosovskihVA.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KosovskihVA.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("15460999-7b1d-4c98-a645-919eae33f15a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KosovskihVA.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KosovskihVA.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.KosovskihVA.Sprint0.Task5.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("А + В = " + DataService.Addition(1, 5));
+            Console.WriteLine("А + В = " + DataService.Subtraction(15, 5));
+            Console.WriteLine("А + В = " + DataService.Multiplication(10, 10));
+            Console.WriteLine("А + В = " + DataService.Division(5, 0));
+            Console.ReadKey();
+        }
+    }
+}
