5100ee85576d812db0a16ff2b2b62a54d0af0114
Tyuiu.kosovskihVA@outlook.com <mr.kosovskih35@mail.ru>
2023-09-29 22:22:48 +0500

    Task2

---
+++ Tyuiu.KosovskihVA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KosovskihVA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KosovskihVA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Влад";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Влад", res);
+        }
+    }
+}
---
+++ Tyuiu.KosovskihVA.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KosovskihVA.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KosovskihVA.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("79825902-8d03-4e67-9dbf-ae543d9f7b69")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
